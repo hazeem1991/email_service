c0bd6d0173ba9e859c0dba2fcad1428c2b65416e
Hazem Arian <hazem@payfull.com>
2019-08-05 04:34:44 +0300

    Start to Docrize the Service

--- app/Http/Libraries/Providers/MainServiceProvider.php
+++ app/Http/Libraries/Providers/MainServiceProvider.php
@@ -3,23 +3,24 @@
 namespace App\Http\Libraries\Providers;
 
 
-use phpDocumentor\Reflection\Types\Self_;
-
 class MainServiceProvider
 {
-    const MESSAGE_TYPES=["plain","html"];
+    const MESSAGE_TYPES = ["plain", "html"];
+
     public function __construct()
     {
     }
+
     /**
      * Get The MailServer That Implemented .
      * @return []
      */
-    public function getSenders():array
+    public function getSenders(): array
     {
-        return explode(",",env('MAIL_PROVIDERS'));
+        return explode(",", env('MAIL_PROVIDERS'));
     }
-    public function getMessageTypes():array
+
+    public function getMessageTypes(): array
     {
         return self::MESSAGE_TYPES;
     }

d95f55a2d9a9726a99b6a86eca7556a6fcf0ca72
Hazem Arian <hazem@payfull.com>
2019-08-05 10:21:16 +0300

    adding bash files for win and linux

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
--- emaildock/bat.bat
+++ emaildock/bat.bat
@@ -1,8 +1,6 @@
-#!/bin/bash
 docker  exec -w /var/www/ EmailService_Core composer install
 docker  exec -w /var/www/ EmailService_Core php artisan migrate
 docker  exec -w /var/www/ EmailService_Core php artisan cache:clear
 docker  exec -w /var/www/ EmailService_Core php artisan queue:restart
 docker  exec -w /var/www/public/front EmailService_Core npm install
 docker  exec -w /var/www/public/front EmailService_Core npm run build
-chmod 777 -R /var/www/storage

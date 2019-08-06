bb98a6952a7463255fed0f56518d388c75c22645
Hazem Arian <hazem@payfull.com>
2019-08-06 10:04:12 +0300

    Mysql

--- emaildock/mysql/docker-entrypoint-initdb.d/init.sql
+++ emaildock/mysql/docker-entrypoint-initdb.d/init.sql
@@ -1,1 +1,1 @@
-CREATE DATABASE IF NOT EXISTS email_service; CREATE DATABASE IF NOT EXISTS email_service_test;
+CREATE DATABASE IF NOT EXISTS email_service; CREATE DATABASE IF NOT EXISTS email_service_test;ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'sqJZGts97vV843huENmN';
